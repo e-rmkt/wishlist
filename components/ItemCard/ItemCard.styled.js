f0acbc00f1df9cd75e8f381b204832756bb9250c
Eliza Romeikat <eliza-romeikat@gmx.de>
2023-10-11 01:16:34 +0200

    style: add styling to ItemCard

--- components/ItemCard/ItemCard.styled.js
+++ components/ItemCard/ItemCard.styled.js
@@ -5,12 +5,26 @@ export const StyledListItem = styled.li`
   list-style-type: none;
   display: flex;
   flex-direction: column;
-  width: fit-content;
-  padding: 12px;
+  flex: 5;
+  width: 13rem;
+  padding: 5px;
   border-radius: 10px;
   border: 1px solid grey;
 `;
 
 export const StyledImage = styled(Image)`
   border-radius: 10px;
+  object-fit: cover;
+`;
+
+export const ImageContainer = styled.div`
+  height: 16rem;
+  position: relative;
+  overflow: hidden;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
 `;
