33dae0f468066d1f6ba26d688d17fdae0d06515d
Eliza Romeikat <eliza-romeikat@gmx.de>
2023-10-12 21:51:06 +0200

    feat: add CreateItem Button

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import ItemList from "@/components/ItemList/ItemList";
+import CreateItem from "@/components/CreateItem";
+import { Header } from "@/components/Layout/Header/Header.styled";
 
 export default function HomePage() {
   return (
     <main>
-      <h1>My Bulletin</h1>
+      <Header>
+        <h1>My Bulletin</h1> <CreateItem />
+      </Header>
       <ItemList />
     </main>
   );
